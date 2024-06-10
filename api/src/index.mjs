df5837eb9bc432bfc48115d7528004b5c041671b
Ngaburiaa <162695052+Ngaburiaa@users.noreply.github.com>
2024-06-10 16:49:49 +0300

    controllers

--- api/src/index.mjs
+++ api/src/index.mjs
@@ -3,9 +3,10 @@ import express from "express";
 
 const app = express();
 
-
 app.use(route);
 
 app.listen(3000 || process.env.PORT, () =>
   console.log("The port is running on port 3000")
 );
+
+
